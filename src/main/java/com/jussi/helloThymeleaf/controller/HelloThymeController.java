8e0f8102dbe979ea5cafc07358a6369121c59a6b
Jussi-Savoranta <jussi.savoranta@gmail.com>
2022-08-30 18:24:13 +0300

    changed controller to use GetMapping annotation

--- src/main/java/com/jussi/helloThymeleaf/controller/HelloThymeController.java
+++ src/main/java/com/jussi/helloThymeleaf/controller/HelloThymeController.java
@@ -2,13 +2,16 @@ package com.jussi.helloThymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloThymeController {
 
-	@RequestMapping("/hello")
+	//@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	@GetMapping(value = "/hello") // shorter way but same as @RequesMapping(..GET)
 	public String sayHello(@RequestParam(value="name") String name, @RequestParam(value="age") int age, Model model) {
 		model.addAttribute("name", name);
 		model.addAttribute("age", age);
